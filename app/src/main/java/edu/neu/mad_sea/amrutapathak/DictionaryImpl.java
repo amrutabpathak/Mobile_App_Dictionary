9eff2a166f924dd478f59a6aa0c8663b03fc24a5
amrutabpathak <pathak.am@husky.neu.edu>
2020-10-04 18:28:27 -0700

    Used Trie to improve performance

--- app/src/main/java/edu/neu/mad_sea/amrutapathak/DictionaryImpl.java
+++ app/src/main/java/edu/neu/mad_sea/amrutapathak/DictionaryImpl.java
@@ -1,6 +1,8 @@
 package edu.neu.mad_sea.amrutapathak;
 
 import android.content.Context;
+import android.icu.text.Edits;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -9,24 +11,35 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 
 import edu.neu.mad_sea.amrutapathak.beans.DictionaryBean;
+import edu.neu.mad_sea.amrutapathak.datastructimpl.Trie;
 import edu.neu.mad_sea.amrutapathak.util.Constants;
 
 public class DictionaryImpl {
- private HashSet<String> dictSet;
+    private static final String TAG = "DictionaryImpl";
+
+    private HashSet<String> dictSet;
  private HashMap<Integer,Character> posChar;
     private HashSet<String> resultSet;
     private List<Character> chList;
+    private Trie t;
+    private HashSet<String> set;
 
 
     public DictionaryImpl(Context c){
         dictSet = new HashSet<>();
         resultSet = new HashSet<>();
+        t = new Trie();
+        set = new HashSet<>();
         buildDictionary(c);
+
 
 }
 
@@ -37,20 +50,25 @@ public void buildDictionary(Context c){
                     new InputStreamReader(c.getAssets().open(Constants.DICTIONARY_PATH)));
 
             while ((l = br.readLine()) != null){
-                dictSet.add(l);
+                t.put(l);
             }
+            System.out.println("File upload success");
+        br.close();
         }catch (FileNotFoundException f){
             System.out.println("File not found");
             f.printStackTrace();
         }catch (Exception e){
             System.out.println("Other error");
             e.printStackTrace();
+        }finally {
         }
     }
 
-    public DictionaryBean getWords(DictionaryBean bean){
-        Set<String> set = new HashSet<String>();
+    public DictionaryBean getWords(DictionaryBean bean) {
+        Log.d(TAG, "getWords: started");
         posChar = new HashMap<>();
+        set = new HashSet<>();
+        resultSet = new HashSet<>();
         String constPattern = bean.getConstraintPattern();
         String allowedWords = bean.getAvailableLetters();
         chList= new ArrayList<>();
@@ -62,37 +80,116 @@ public void buildDictionary(Context c){
                 }
             }
         }
-
+        Log.d(TAG, "getWords: "+posChar.toString());
         for(Character ch : allowedWords.toCharArray()){
             chList.add(ch);
         }
-        recurse(posChar,chList,0,bean.getWordLength(),0,new StringBuilder());
-        bean.setResultingWordSet(set);
+      //  recurse(posChar,chList,0,bean.getWordLength(),0,new StringBuilder());
+        List<List<Character>> res = getAllPermutations(chList,bean.getWordLength());
+        StringBuilder sb = new StringBuilder();
+
+        for(List<Character> l : res){
+            sb = new StringBuilder();
+            for(int i = 0; i < bean.getWordLength(); i++){
+                if(posChar.containsKey(i) && posChar.get(i) != l.get(i)){
+                    sb = null;
+                    break;
+                }
+                sb.append(l.get(i));
+            }
+            if(sb!= null){
+                set.add(sb.toString());
+            }
+        }
+
+        Iterator<String> itr = set.iterator();
+        while(itr.hasNext()){
+           // Log.d(TAG, "getWords: Inside set");
+            String str = itr.next();
+            if(t.isWordPresent(str)){
+                resultSet.add(str);
+            }
+        }
+        Log.d(TAG, "getWords: resultSet"+set);
+        bean.setResultingWordSet(resultSet);
+        Log.d(TAG, "getWords: "+bean.getResultingWordSet().toString());
         return bean;
 
     }
 
-    private void recurse(HashMap<Integer,Character> posChar, List<Character> allowedLetters,int allowedLetterPos, int wordLength, int pos,StringBuilder sb ){
-        if(pos == wordLength -1){
-            resultSet.add(sb.toString());
+    /* private void recurse(HashMap<Integer,Character> posChar, List<Character> allowedLetters,int allowedLetterPos, int wordLength, int pos,StringBuilder sb ){
+        if(pos == wordLength ){
+            set.add(sb.toString());
+            Log.d(TAG, "recurse: inside check");
+
         }
 
-        if(pos >= wordLength || allowedLetterPos >=  wordLength){
+        if(pos >= wordLength ){
             return;
         }
 
         for(int i = allowedLetterPos; i < allowedLetters.size(); i++){
            if(posChar.containsKey(i)){
                sb.append(posChar.get(i));
+
            }else{
                sb.append(allowedLetters.get(i));
-           }
+               recurse(posChar,allowedLetters, allowedLetterPos+1,wordLength,pos+1,sb);
 
-           recurse(posChar,allowedLetters, allowedLetterPos+1,wordLength,pos+1,sb);
+           }
+            recurse(posChar,allowedLetters, allowedLetterPos,wordLength,pos+1,sb);
+
+           sb.deleteCharAt(sb.length()-1);
         }
 
 
+    } */
+/*
+private void recurse(HashMap<Integer,Character> posChar,List<Character> allowedLetters,int allowedLetterPos,  int wordLength, int pos, StringBuilder sb  ){
+    if(pos == wordLength ){
+        return;
+        set.add(sb.toString());
+        Log.d(TAG, "recurse: inside check");
+
     }
 
+    if(allowedLetterPos >= allowedLetters.size()){
+        return;
+    }
 
+    if(posChar.containsKey(pos-1)){
+        sb.append(posChar.get(pos-1));
+        recurse(posChar, allowedLetters, allowedLetterPos, wordLength, pos+1, sb);
+    }else{
+        recurse(posChar, allowedLetters, allowedLetterPos+1, wordLength, pos, sb);
+        sb.append(allowedLetters.get(allowedLetterPos));
+        recurse(posChar, allowedLetters, allowedLetterPos+1, wordLength, pos+1, sb);
+
+    }
+
+    sb.deleteCharAt(sb.length()-1);
+}
+*/
+    public  List<List<Character>> getAllPermutations(List<Character> allowedLetters,int wordLength) {
+        List<List<Character>> output = new ArrayList<>();
+        Queue<List<Character>> pQueue = new LinkedList<>();
+        pQueue.add(new ArrayList<Character>());
+        for (Character c : allowedLetters) {
+            // we will take all existing permutations and add the current number to create new permutations
+            int n = pQueue.size();
+            for (int i = 0; i < n; i++) {
+                List<Character> prevList = pQueue.poll();
+                // create a new permutation by adding the current number at every position
+                for (int j = 0; j <= prevList.size(); j++) {
+                    List<Character> newPermutation = new ArrayList<Character>(prevList);
+                    newPermutation.add(j, c);
+                    if (newPermutation.size() == allowedLetters.size())
+                        output.add(newPermutation);
+                    else
+                        pQueue.add(newPermutation);
+                }
+            }
+        }
+        return output;
+    }
 }
--- app/src/main/java/edu/neu/mad_sea/amrutapathak/TestDictionaryActivity.java
+++ app/src/main/java/edu/neu/mad_sea/amrutapathak/TestDictionaryActivity.java
@@ -2,6 +2,8 @@ package edu.neu.mad_sea.amrutapathak;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -9,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +26,12 @@ public class TestDictionaryActivity extends AppCompatActivity {
     private DictionaryBean dictionaryBean;
     private ListView lvDictResults;
     private ArrayAdapter arrAdapter;
+    private EditText etConstraintText;
+    private Button  btnFind;
+    private TextView tvTotalTime;
+    private long startTime;
+    private long endTime;
+
 
 
 
@@ -35,10 +44,11 @@ public class TestDictionaryActivity extends AppCompatActivity {
     }
 
     private void createComponents(){
-        Button btnFind = findViewById(R.id.btnSubmit);
+         btnFind = findViewById(R.id.btnSubmit);
         btnFind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                startTime = System.currentTimeMillis();
                 getResultingWords();
             }
         });
@@ -59,6 +69,27 @@ public class TestDictionaryActivity extends AppCompatActivity {
             }
         });
 
+         etConstraintText = findViewById(R.id.etconstraintText);
+        etConstraintText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                enableFind();
+            }
+        });
+        tvTotalTime = (TextView) findViewById(R.id.totalTimeId);
+        tvTotalTime.setVisibility(View.GONE);
+        lvDictResults = (ListView) findViewById(R.id.lvResult);
+
         final Spinner snr = (Spinner) findViewById(R.id.spnrNumberofLetters);
         ArrayAdapter<CharSequence> spnrAdptr = ArrayAdapter.createFromResource(this,
                  R.array.numbers, R.layout.support_simple_spinner_dropdown_item);
@@ -69,23 +100,45 @@ public class TestDictionaryActivity extends AppCompatActivity {
 
     }
 
-    private void getResultingWords(){
-        dictionaryBean = new DictionaryBean();
-
-          validateAndPopulateData();
-        dictionaryBean = DictSingleton.getSingle_instance(getApplicationContext()).getWords(dictionaryBean);
-        if(dictionaryBean.getResultingWordSet() == null || dictionaryBean.getResultingWordSet().size() == 0){
-            Toast.makeText(this,"No results found for this search! Please try with different search parameters",Toast.LENGTH_LONG);
-            return;
+    private void enableFind(){
+        boolean canFind = false;
+        if(etConstraintText.getText()!= null && etConstraintText.getText().toString()!= null && etConstraintText.getText().toString().length()!=0){
+            canFind = true;
         }
+        btnFind.setEnabled(canFind);
+    }
+    private void getResultingWords(){
 
-        lvDictResults = (ListView)findViewById(R.id.lvResult);
-        arrAdapter = new ArrayAdapter(TestDictionaryActivity.this, android.R.layout.simple_list_item_1, new ArrayList<String>(dictionaryBean.getResultingWordSet()));
-        lvDictResults.setAdapter(arrAdapter);
+            dictionaryBean = new DictionaryBean();
+
+            if(!validateAndPopulateData()){
+                Toast.makeText(this, "Aww Snap ..Something went wrong. Please check your inputs", Toast.LENGTH_LONG);
+                return;
+            }
+            dictionaryBean = DictSingleton.getSingle_instance(getApplicationContext()).getWords(dictionaryBean);
+            if (dictionaryBean.getResultingWordSet() == null || dictionaryBean.getResultingWordSet().size() == 0) {
+                Toast.makeText(this, "No results found for this search! Please try with different search parameters", Toast.LENGTH_LONG);
+                return;
+            }
+
+
+            arrAdapter = new ArrayAdapter(TestDictionaryActivity.this, android.R.layout.simple_list_item_1, new ArrayList<String>(dictionaryBean.getResultingWordSet()));
+            lvDictResults.setAdapter(arrAdapter);
+            lvDictResults.setVisibility(View.VISIBLE);
+            endTime = System.currentTimeMillis();
+            tvTotalTime.setText(totalTimeTaken(startTime,endTime));
+            tvTotalTime.setVisibility(View.VISIBLE);
+
+
+
 
     }
 
-    private  void validateAndPopulateData(){
+    private String totalTimeTaken(long startTime, long endTime){
+        return "Total time taken "+(endTime-startTime)+ " ms.";
+    }
+
+    private  boolean validateAndPopulateData(){
 
         Spinner wordLengthSpinner = findViewById(R.id.spnrNumberofLetters);
         String wordLengthStr = wordLengthSpinner.getSelectedItem().toString();
@@ -95,20 +148,20 @@ public class TestDictionaryActivity extends AppCompatActivity {
         String availableLetter = etAvailableLetters.getText().toString();
         boolean areLettersValid = validateLetters(availableLetter);
         if(!areLettersValid){
-            return ;
+            return false;
         }
 
         EditText etConstraintPattern = findViewById(R.id.etLetterPlacement);
         String constPattern = etConstraintPattern.getText().toString();
-        boolean isConstPatternValid = validateConstraint(constPattern);
+        boolean isConstPatternValid = validateConstraint(constPattern, availableLetter);
         if(!isConstPatternValid){
-            return ;
+            return false;
         }
 
         dictionaryBean.setWordLength(Integer.parseInt(wordLengthStr));
         dictionaryBean.setAvailableLetters(availableLetter.toLowerCase());
         dictionaryBean.setConstraintPattern(constPattern.toLowerCase());
-        return ;
+        return true;
     }
 
     private boolean validateLetters(String availableLetter){
@@ -128,18 +181,28 @@ public class TestDictionaryActivity extends AppCompatActivity {
         return true;
     }
 
-    private boolean validateConstraint(String constPattern) {
+    private boolean validateConstraint(String constPattern , String availableLetter) {
         if(constPattern != null ){
+            Log.d(TAG, "validateConstraint: started");
             for(Character c : constPattern.toCharArray()){
-                if(Character.isLetter(c) || Character.isSpaceChar(c)){
+
+                if(c!='_' && !availableLetter.contains(c+"")){
+                    return false;
+                }
+
+                if(availableLetter!= null && availableLetter.length() < constPattern.length()){
+                    return false;
+                }
+                if(Character.isLetter(c) || c == '_'){
                     continue;
                 }else{
                     Toast.makeText(this,"Please enter valid alphabets!",Toast.LENGTH_LONG);
                     return false;
                 }
+
             }
         }
-
+        Log.d(TAG, "validateConstraint: " );
         return true;
     }
 
@@ -150,8 +213,6 @@ public class TestDictionaryActivity extends AppCompatActivity {
     }
 
     private void clear(){
-        //.setVisibility(View.GONE);
-        //.setVisibility(View.GONE);
 
         EditText constLetters =
                 (EditText) findViewById(R.id.etconstraintText);
@@ -163,6 +224,14 @@ public class TestDictionaryActivity extends AppCompatActivity {
 
         Spinner spnrNumberofLetters = (Spinner) findViewById(R.id.spnrNumberofLetters);
         spnrNumberofLetters.setSelection(0);
+
+        lvDictResults.setVisibility(View.GONE);
+
+        tvTotalTime.setVisibility(View.GONE);
+
+
+
+
 
     }
 
